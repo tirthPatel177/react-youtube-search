3452c7d451b259e19b3f1fd4f24fb62f59a7ef29
Tirth Patel <177tpatel@gmail.com>
2021-07-06 16:54:50 +0530

    Completed the Build

--- src/components/api.js
+++ src/components/api.js
@@ -3,13 +3,13 @@ const KEY = "AIzaSyD_J_KR7_TTMfI1vvK6yle3I1l2C3JB3RE";
 
 const YoutubeSearch =
         axios.create({
-            baseURL: 'https://www.googleapis.com/youtube/v3/',
+            baseURL: 'YouTube API KEY',
             params: {
                 part: 'snippet',
                 maxResults: 7,
                 key: KEY
             }
         })
-        
+
 
 export default YoutubeSearch;
